ad0947c4d24600dde25a62e83ade2cf24294bf76
harsanyiz <34844766+harsanyiz@users.noreply.github.com>
2026-04-22 19:00:20 +0200

    Update ui_weather.py

--- ui_weather.py
+++ ui_weather.py
@@ -1,64 +1,3 @@
-from PIL import Image, ImageDraw, ImageFont, ImageStat
-from datetime import datetime
-import os
-
-# --- KONFIGURÁCIÓ & MÉRETEK (4K HORIZONTÁLIS) - LUXUS KÍNAI EV STYLE ---
-WIDGET_WIDTH, WIDGET_HEIGHT, WIDGET_Y, OFFSET_LEFT, INNER_MARGIN = 2400, 200, 100, 135, 80
-FONT_TEMP, FONT_HEADER, FONT_VALUE, FONT_SMALL, FONT_DATETIME, FONT_NAME = 104, 24, 36, 30, 22, 28
-FONT_FORECAST_DAY, FONT_FORECAST_TEMP = 22, 32
-ICON_DISPLAY_SIZE, WIND_ICON_SIZE, PARA_ICON_SIZE, FORECAST_ICON_SIZE, SUN_ICON_SIZE = 160, 36, 36, 50, 40
-
-def find_font(bold=False, heavy=False):
-    """San Francisco betű keresése a Data/Fonts mappában"""
-    
-    # A feltöltött fájlok alapján a helyes elérési út
-    font_dir = "Data/Fonts"
-    
-    # Betűválasztás súlyozás szerint
-    if bold:
-        font_name = "SFProText-Bold.ttf"
-    elif heavy:
-        font_name = "SFProText-Heavy.ttf"
-    else:
-        font_name = "SFProText-Regular.ttf"
-    
-    font_path = os.path.join(font_dir, font_name)
-    
-    if os.path.exists(font_path):
-        return font_path
-    
-    # Tartalék: próbálkozz más változatokkal
-    fallbacks = [
-        os.path.join(font_dir, "SFProText-Medium.ttf"),
-        os.path.join(font_dir, "SFProText-Semibold.ttf"),
-        "/usr/share/fonts/truetype/noto/NotoSans-Bold.ttf" if bold else "/usr/share/fonts/truetype/noto/NotoSans-Regular.ttf",
-        "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf" if bold else "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",
-    ]
-    
-    for fb in fallbacks:
-        if os.path.exists(fb):
-            return fb
-    
-    return None
-
-def get_f(size, bold=False, heavy=False):
-    """Betűtípus betöltése San Franciscóval"""
-    path = find_font(bold=bold, heavy=heavy)
-    if path:
-        size = size if size % 2 == 0 else size + 1  # párosra kerekítés az élességért
-        font = ImageFont.truetype(path, size)
-        try:
-            font.fontmode = "RGB"  # RGB anti-aliasing
-        except:
-            pass
-        return font
-    return ImageFont.load_default()
-
-def paste_icon(img, icon_img, x, y, size=ICON_DISPLAY_SIZE):
-    if icon_img:
-        resized = icon_img.resize((size, size), Image.Resampling.LANCZOS)
-        img.paste(resized, (int(x), int(y)), resized)
-
 def draw_weather_widget(img, weather, icon_img, feel_icon_img, rainq_icon_img, wind1_icon_img, para_icon_img, forecast_icons, sunrise_icon_img, sunset_icon_img, namedays, tz_offset):
     from logic_weather import get_day_hu
     bx, by, bw, bh = OFFSET_LEFT, WIDGET_Y, WIDGET_WIDTH, WIDGET_HEIGHT
@@ -89,27 +28,28 @@ def draw_weather_widget(img, weather, icon_img, feel_icon_img, rainq_icon_img, w
     
     draw = ImageDraw.Draw(img)
     
-    # GRADIENT HÁTTÉR - csak a widget területére
+    # GRADIENT HÁTTÉR - javítva: 10 pixel balra tolás
+    gradient_start_x = bx - 10  # 10 pixel balra
     for y in range(by, by + bh):
         ratio = (y - by) / bh
         r = int(colors["bg_start"][0] * (1 - ratio) + colors["bg_end"][0] * ratio)
         g = int(colors["bg_start"][1] * (1 - ratio) + colors["bg_end"][1] * ratio)
         b = int(colors["bg_start"][2] * (1 - ratio) + colors["bg_end"][2] * ratio)
         a = colors["bg_start"][3]
-        draw.line([(bx, y), (bx + bw, y)], fill=(r, g, b, a), width=1)
+        draw.line([(gradient_start_x, y), (gradient_start_x + bw, y)], fill=(r, g, b, a), width=1)
     
     mid_y = by + (bh // 2)
     curr_x = bx + INNER_MARGIN
     
     # Betűtípusok - a fontosabbakhoz Heavy, a többiekhez Bold vagy Regular
-    f_t = get_f(FONT_TEMP, heavy=True)      # Hőfokhoz Heavy (vastag, jól látszik)
-    f_h = get_f(FONT_HEADER, bold=True)     # Fejléchez Bold
-    f_v = get_f(FONT_VALUE, heavy=True)     # Értékekhez Heavy
-    f_s = get_f(FONT_SMALL)                 # Kisebb szöveghez Regular
-    f_dt = get_f(FONT_DATETIME)             # Dátumhoz Regular
-    f_n = get_f(FONT_NAME, bold=True)       # Névnaphoz Bold
-    f_fd = get_f(FONT_FORECAST_DAY, bold=True)  # Nap neve Bold
-    f_fv = get_f(FONT_FORECAST_TEMP, heavy=True) # Előrejelzés hőfok Heavy
+    f_t = get_f(FONT_TEMP, heavy=True)
+    f_h = get_f(FONT_HEADER, bold=True)
+    f_v = get_f(FONT_VALUE, heavy=True)
+    f_s = get_f(FONT_SMALL)
+    f_dt = get_f(FONT_DATETIME)
+    f_n = get_f(FONT_NAME, bold=True)
+    f_fd = get_f(FONT_FORECAST_DAY, bold=True)
+    f_fv = get_f(FONT_FORECAST_TEMP, heavy=True)
 
     # 1. AKTUÁLIS BLOKK (HŐFOK + ESŐ/ÉRZET EGYMÁS FELETT)
     SEC1_W = 820
